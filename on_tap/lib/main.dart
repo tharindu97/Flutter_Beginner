117cdf7646765578e81c214b76244fe06618e6e3
tharindu97 <tharindukavishna@gmail.com>
2020-05-01 17:48:31 +0530

    simple_on_tab

---
+++ on_tap/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Tab',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return DefaultTabController(
+      length: 3, 
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          bottom: TabBar(
+            unselectedLabelColor: Colors.redAccent,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.redAccent
+            ),
+            tabs: [
+              Tab(
+                text: 'APPS',
+              ),
+              Tab(text: 'MOVIES',),
+              Tab(text: 'GAMES',),
+            ] 
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Apps')
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Movies')
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Games')
+                  ],
+                ),
+              ),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
