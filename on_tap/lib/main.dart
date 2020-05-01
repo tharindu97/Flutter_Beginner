992f36a91479c166c73e2b88160574346242257e
tharindu97 <tharindukavishna@gmail.com>
2020-05-01 17:59:58 +0530

    on_tab_item_one

--- on_tap/lib/main.dart
+++ on_tap/lib/main.dart
@@ -41,10 +41,50 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             tabs: [
               Tab(
-                text: 'APPS',
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      color: Colors.redAccent,
+                      width: 1
+                    )
+                  ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('APPS'),
+                  ),
+                ),
               ),
-              Tab(text: 'MOVIES',),
-              Tab(text: 'GAMES',),
+              Tab(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      color: Colors.redAccent,
+                      width: 1
+                    )
+                  ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('MOVIES'),
+                  ),
+                ),
+              ),
+              Tab(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      color: Colors.redAccent,
+                      width: 1
+                    )
+                  ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('GAMES'),
+                  ),
+                ),
+              ),
             ] 
           ),
         ),
