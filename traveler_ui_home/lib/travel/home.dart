7e94f1b51aadf57435250a97cacf6d6f9e6ad9cc
tharindu97 <tharindukavishna@gmail.com>
2020-05-01 22:53:57 +0530

    travel_ui_templete_completed

--- traveler_ui_home/lib/travel/home.dart
+++ traveler_ui_home/lib/travel/home.dart
@@ -6,27 +6,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       body: Container(
-         color: Colors.green,
-         child: Center(
-           child: Column(
-             children: <Widget>[
-               SizedBox(
-                 height: 350,
-                 child: Container(
-                   decoration: BoxDecoration(
-                     image: DecorationImage(image: AssetImage('panama.png')),
-                   ),
-                   padding: EdgeInsets.only(top:200),
-                   color: Colors.white,
-                   child: Column(
-                     mainAxisAlignment: MainAxisAlignment.center,
+    return DefaultTabController(length: 3, 
+    child: Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(250),
+          child: AppBar(
+          backgroundColor:Colors.cyanAccent,
+          leading: IconButton(
+                icon: Icon(Icons.navigate_before),
+                tooltip: 'Next page',
+                onPressed: () {},
+          ),
+          flexibleSpace: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('image/emma.jpg'),
+                  fit: BoxFit.fitHeight
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.only(top: 110)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                      children: <Widget>[
                        Text('Thrindu Kavishna \npanama Srilanka'),
-                       Padding(padding: EdgeInsets.only(top: 20)),
+                       Padding(padding: EdgeInsets.only(top: 10)),
                        Row(
                          mainAxisAlignment: MainAxisAlignment.center,
                          children: <Widget>[
@@ -34,7 +42,7 @@ class _HomePageState extends State<HomePage> {
                              child: Text('Message'),
                              onPressed: null
                              ),
-                             Padding(padding: EdgeInsets.only(left: 150)),
+                            Padding(padding: EdgeInsets.only(left: 150)),
                             Column(
                               children: <Widget>[
                                 RaisedButton(
@@ -48,39 +56,97 @@ class _HomePageState extends State<HomePage> {
                        )
                      ],
                    ),
-                 ),
-               ),
-               SizedBox(
-                 height: 30,
-                 child: Container(
-                   color: Colors.red,
-                   child: Center(
-                     child: Row(
-                       children: <Widget>[
-                        Padding(padding: EdgeInsets.only(left: 30)),
-                        RaisedButton(
-                          child: Text('ABOUT'),
-                          onPressed: null
-                          ),
-                        Padding(padding: EdgeInsets.only(left: 30)),
-                        RaisedButton(
-                          child: Text('FAVOURITES'),
-                          onPressed: null
-                          ),
-                        Padding(padding: EdgeInsets.only(left: 30)),
-                        RaisedButton(
-                          child: Text('GALLERY'),
-                          onPressed: null
-                          ),
-                       ],
-                     ),
-                   ),
-                 ),
-               )
-             ],
+                ]
+              )
            ),
-         ),
-       ),
-    );
+          bottom: TabBar(
+            unselectedLabelColor: Colors.grey,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicator: BoxDecoration(
+              //color: Colors.greenAccent
+            ),
+            tabs: [
+              Tab(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('ABOUT'),
+                  ),
+                ),
+                Tab(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('FAVOURITES'),
+                  ),
+                ),
+                Tab(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('GALLERY'),
+                  ),
+                ),
+            ]
+          ),
+        ),
+      ),
+      body: TabBarView(
+        children: [
+          Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('About')
+                  ],
+                ),
+              ),
+            ),
+          Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Favourites')
+                  ],
+                ),
+              ),
+            ),
+          Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Gallery')
+                  ],
+                ),
+              ),
+            ),
+        ]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blueAccent,
+        onTap: null,
+        items:[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,
+              color: Colors.white,
+            ),
+            title: Text('data') 
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.person,
+              color: Colors.white,
+            ),
+            title: Text('data') 
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.mail,
+              color: Colors.white,
+            ),
+            title: Text('data') 
+          ),
+        ] 
+      ),
+    )
+  );
   }
 }
